e0e274f8159f6757f737bf38032315bfa719dcae
Jake Neverida <jrneverida@up.edu.ph>
2023-11-22 16:41:32 +0800

    Fixed main.java

--- MazeQuest/src/application/Main.java
+++ MazeQuest/src/application/Main.java
@@ -1,23 +1,26 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
-import mainGame.Game;
 
 public class Main extends Application 
 {
-		
-    public static void main(String[] args) 
-    {
-        launch(args);
-    }
-
-    @Override
-    public void start(Stage stage) 
-    {
-       Game game = new Game();
-       game.setStage(stage);
-    }
-    
-    
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			BorderPane root = new BorderPane();
+			Scene scene = new Scene(root,400,400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
